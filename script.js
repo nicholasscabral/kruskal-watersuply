26a52f0f61035233a8d2c227b9991599cd418b5e
Nicholas Cabral <nicholasscabral@gmail.com>
2023-05-23 21:57:56 -0300

    refactor: move callbacks into functions

--- script.js
+++ script.js
@@ -13,15 +13,119 @@ const defaultEdges = [
   // { data: { source: "D", target: "E", weight: 10 } },
 ];
 
-const calculateDistance = (node1, node2) => {
+let sourceNodeId = null;
+let sourceNode = null;
+let lastNodeId = defaultNodes.at(-1).data.id;
+
+function calculateDistance(node1, node2) {
   const pos1 = node1.position();
   const pos2 = node2.position();
   const dx = Math.abs(pos1.x - pos2.x);
   const dy = Math.abs(pos1.y - pos2.y);
   const distance = Math.sqrt(dx * dx + dy * dy);
-  console.log({ distance, a: parseInt(distance) });
-  return Math.floor(parseInt(distance / 50));
-};
+  return Math.floor(distance / 50);
+}
+
+function updateEdgesWeights(selectedNode) {
+  const connectedEdges = selectedNode.connectedEdges();
+
+  connectedEdges.forEach((edge) => {
+    const sourceNode = edge.source();
+    const targetNode = edge.target();
+    const distance = calculateDistance(sourceNode, targetNode);
+
+    edge.data("weight", distance);
+  });
+
+  cy.style().update();
+}
+
+function createEdge(event) {
+  const node = event.target;
+
+  if (sourceNodeId === null) {
+    sourceNodeId = node.id();
+    sourceNode = node;
+    sourceNode.addClass("selected");
+  } else {
+    const newEdgeId = "NewEdge_" + Date.now();
+
+    const targetNodeId = node.id();
+
+    cy.add({
+      data: {
+        id: newEdgeId,
+        source: sourceNodeId,
+        target: targetNodeId,
+        weight: calculateDistance(sourceNode, node),
+      },
+    });
+
+    sourceNodeId = null;
+    sourceNode.removeClass("selected");
+  }
+}
+
+function createNode(event) {
+  if (event.target === cy) {
+    const newNodeId = String.fromCharCode(lastNodeId.charCodeAt(0) + 1);
+
+    const position = event.position || event.cyPosition;
+
+    cy.add({
+      data: { id: newNodeId, name: "New Node" },
+      position: { x: position.x, y: position.y },
+    });
+
+    if (sourceNodeId !== null) {
+      const newEdgeId = "NewEdge_" + Date.now();
+
+      cy.add({
+        data: { id: newEdgeId, source: sourceNodeId, target: newNodeId },
+      });
+
+      sourceNodeId = null;
+    }
+
+    lastNodeId = newNodeId;
+
+    const nodeEdges = {};
+
+    cy.elements()
+      .edges()
+      .forEach((edge) => {
+        const sourceNodeId = edge.source().id();
+        const targetNodeId = edge.target().id();
+        const weight = edge.data("weight");
+
+        // Create an object with weight and targetNodeId
+        const edgeObj = { weight, targetNodeId };
+
+        // Add edge object to the source node's array
+        if (nodeEdges[sourceNodeId]) {
+          nodeEdges[sourceNodeId].push(edgeObj);
+        } else {
+          nodeEdges[sourceNodeId] = [edgeObj];
+        }
+
+        // Add edge object to the target node's array
+        if (nodeEdges[targetNodeId]) {
+          nodeEdges[targetNodeId].push(edgeObj);
+        } else {
+          nodeEdges[targetNodeId] = [edgeObj];
+        }
+      });
+
+    console.log(nodeEdges);
+
+    cy.layout().run();
+  }
+}
+
+function handleNodeMove(event) {
+  const selectedNode = event.target;
+  updateEdgesWeights(selectedNode);
+}
 
 let cy = cytoscape({
   container: document.getElementById("cy"),
@@ -85,103 +189,6 @@ let cy = cytoscape({
   },
 });
 
-let sourceNodeId = null;
-let sourceNode = null;
-let lastNodeId = defaultNodes.at(-1).data.id;
-
-cy.on("tap", "node", function (event) {
-  const node = event.target;
-
-  if (sourceNodeId === null) {
-    sourceNodeId = node.id();
-    sourceNode = node;
-    sourceNode.addClass("selected");
-  } else {
-    const newEdgeId = "NewEdge_" + Date.now();
-
-    const targetNodeId = node.id();
-
-    cy.add({
-      data: {
-        id: newEdgeId,
-        source: sourceNodeId,
-        target: targetNodeId,
-        weight: calculateDistance(sourceNode, node),
-      },
-    });
-
-    sourceNodeId = null;
-    sourceNode.removeClass("selected");
-  }
-});
-
-cy.on("tap", function (event) {
-  if (event.target === cy) {
-    const newNodeId = String.fromCharCode(lastNodeId.charCodeAt(0) + 1);
-
-    const position = event.position || event.cyPosition;
-
-    cy.add({
-      data: { id: newNodeId, name: "New Node" },
-      position: { x: position.x, y: position.y },
-    });
-
-    if (sourceNodeId !== null) {
-      const newEdgeId = "NewEdge_" + Date.now();
-
-      cy.add({
-        data: { id: newEdgeId, source: sourceNodeId, target: newNodeId },
-      });
-
-      sourceNodeId = null;
-    }
-
-    lastNodeId = newNodeId;
-
-    const nodeEdges = {};
-
-    cy.elements()
-      .edges()
-      .forEach((edge) => {
-        const sourceNodeId = edge.source().id();
-        const targetNodeId = edge.target().id();
-        const weight = edge.data("weight");
-
-        // Create an object with weight and targetNodeId
-        const edgeObj = { weight, targetNodeId };
-
-        // Add edge object to the source node's array
-        if (nodeEdges[sourceNodeId]) {
-          nodeEdges[sourceNodeId].push(edgeObj);
-        } else {
-          nodeEdges[sourceNodeId] = [edgeObj];
-        }
-
-        // Add edge object to the target node's array
-        if (nodeEdges[targetNodeId]) {
-          nodeEdges[targetNodeId].push(edgeObj);
-        } else {
-          nodeEdges[targetNodeId] = [edgeObj];
-        }
-      });
-
-    console.log(nodeEdges);
-
-    cy.layout().run();
-  }
-});
-
-cy.on("position", "node", function (event) {
-  const node = event.target;
-  const connectedEdges = node.connectedEdges();
-
-  connectedEdges.forEach((edge) => {
-    const sourceNode = edge.source();
-    const targetNode = edge.target();
-    const distance = calculateDistance(sourceNode, targetNode);
-
-    edge.data("weight", distance.toFixed(2));
-  });
-
-  cy.style().update();
-});
+cy.on("tap", "node", createEdge);
+cy.on("tap", createNode);
+cy.on("position", "node", handleNodeMove);
